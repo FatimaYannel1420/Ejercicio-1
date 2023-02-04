a9bac5af09919dff9aa96ab29302a85e82f9e542
FatimaYannel1420 <123432924+FatimaYannel1420@users.noreply.github.com>
2023-02-03 19:32:41 -0600

    Procedimiento

---
+++ Tarea1_Lenguaje3/Tarea1_Lenguaje3/Ejercicio1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Tarea1_Lenguaje3
+{
+    partial class Ejercicio1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_numero1 = new System.Windows.Forms.TextBox();
+            this.Btn_aceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(398, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INGRESE UN NUMERO";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NUMERO";
+            // 
+            // txt_numero1
+            // 
+            this.txt_numero1.Location = new System.Drawing.Point(232, 97);
+            this.txt_numero1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txt_numero1.Name = "txt_numero1";
+            this.txt_numero1.Size = new System.Drawing.Size(164, 26);
+            this.txt_numero1.TabIndex = 2;
+            // 
+            // Btn_aceptar
+            // 
+            this.Btn_aceptar.Location = new System.Drawing.Point(401, 188);
+            this.Btn_aceptar.Name = "Btn_aceptar";
+            this.Btn_aceptar.Size = new System.Drawing.Size(109, 30);
+            this.Btn_aceptar.TabIndex = 3;
+            this.Btn_aceptar.Text = "Aceptar";
+            this.Btn_aceptar.UseVisualStyleBackColor = true;
+            this.Btn_aceptar.Click += new System.EventHandler(this.Btn_aceptar_Click);
+            // 
+            // Ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1012, 456);
+            this.Controls.Add(this.Btn_aceptar);
+            this.Controls.Add(this.txt_numero1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Ejercicio1";
+            this.Text = "Ejercicio1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_numero1;
+        private System.Windows.Forms.Button Btn_aceptar;
+    }
+}
+
---
+++ Tarea1_Lenguaje3/Tarea1_Lenguaje3/Ejercicio1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea1_Lenguaje3
+{
+    public partial class Ejercicio1 : Form
+    {
+        public Ejercicio1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn_aceptar_Click(object sender, EventArgs e)
+        {
+            decimal numero = Convert.ToDecimal(txt_numero1.Text);
+            numeropar(numero);
+
+        }
+        
+        
+        private decimal numeropar (decimal numero)
+        {
+            decimal resultado;
+            resultado = Convert.ToDecimal(txt_numero1.Text);
+            if (numero % 2 == 0)
+            {
+
+                MessageBox.Show("El numero es par");
+            }
+            else
+            {
+                MessageBox.Show("El numero es Impar");
+            }
+            return resultado;
+
+        }
+    }
+    
+}
